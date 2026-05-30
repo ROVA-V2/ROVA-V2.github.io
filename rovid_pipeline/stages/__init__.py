320dfbdbbbb06ee6873f655d784010be06cefa25
LuckyyyyBoyyy <156729671+LuckyyyyBoyyy@users.noreply.github.com>
2026-05-30 11:30:06 +0800

    Delete rovid_pipeline directory

--- rovid_pipeline/stages/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Robust-TO pipeline stages (Fig. 2): perception (Sec 3.2) and synthesis (Sec 3.1)."""
-from .perception import DisturbanceAwarePerception, PerceptionOutput, Fact
-from .synthesis import ConfidenceWeightedSynthesis, SynthesisOutput
-
-__all__ = [
-    "DisturbanceAwarePerception", "PerceptionOutput", "Fact",
-    "ConfidenceWeightedSynthesis", "SynthesisOutput",
-]
